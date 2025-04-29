cb48d89391ca9572fe473ab2b83810f23f700328
rafaelborto <jrf.bortoletto@outlook.com>
2025-04-29 10:20:05 +0200

    math.trunc
    exercicio 17

---
+++ GAFANHOTO/exc16NumbersIntPart.py
@@ -0,0 +1,10 @@
+
+n = float(input("Digite um número real: "))
+int_part = int(n)  # Obtém a parte inteira do número
+print(f"A parte inteira de {n} é {int_part}. (CONVERTENDO FLOAT PARA INT).")
+
+
+import math
+n2 = float(input("Digite um número real: "))
+int_part2 = math.trunc(n2) # Obtém a parte inteira do número
+print(f"A parte inteira de {n2} é {int_part2}. (UTILIZANDO MATH.TRUNC).")
